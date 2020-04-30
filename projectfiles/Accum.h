62cd3a7dc4d8d798c04b49c4a7836d328b9c722f
ligaroba <ligaroba@gmail.com>
2020-04-30 18:39:27 +0300

    Adding Pointer and Referrences code

---
+++ projectfiles/Accum.h
@@ -0,0 +1,8 @@
+//
+// Created by RLIGAYE on 4/28/2020.
+//
+
+#ifndef ALGORITHMS_ACCUM_H
+#define ALGORITHMS_ACCUM_H
+
+#endif //ALGORITHMS_ACCUM_H
---
+++ projectfiles/Utility.h
@@ -0,0 +1,14 @@
+//
+// Created by RLIGAYE on 4/27/2020.
+//
+
+#ifndef ALGORITHMS_UTILITY_H
+#define ALGORITHMS_UTILITY_H
+
+
+class Utility {
+
+};
+
+
+#endif //ALGORITHMS_UTILITY_H
